b8480c494b029d2d7b620227cd1f39b81f602a56
eloregnier <elodie.regnier.92@gmail.com>
2024-04-30 15:43:57 +0200

    header ok

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>HelloWesh</h1>
+      <h3>La bonne cuisine des familles</h3>
+    </div>
+  );
+}
+
+export default Header;
